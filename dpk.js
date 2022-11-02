d1639c52b47d4d7556479fabf30c39ae9a21dfdf
alisson.oliveira@git.sicoob.com.br <alisson.oliveira@sicoob.com.br>
2022-11-02 20:25:48 -0300

    first commit Ticket breakdown

---
+++ dpk.js
@@ -0,0 +1,28 @@
+const crypto = require("crypto");
+
+exports.deterministicPartitionKey = (e) => {
+  const TRIVIAL_PARTITION_KEY = "0";
+  const MAX_PARTITION_KEY_LENGTH = 256;
+  let candidate;
+
+  if (e && e?.partitionKey) {
+    const field = JSON.stringify(e);
+    candidate = e.partitionKey
+      ? e.partitionKey
+      : crypto.createHash("sha3-512").update(field).digest("hex");
+  } else {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
+  if (candidate) {
+    if (typeof candidate !== "string") {
+      candidate = JSON.stringify(candidate);
+    }
+  } else {
+    candidate = TRIVIAL_PARTITION_KEY;
+  }
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
+  return candidate;
+};
